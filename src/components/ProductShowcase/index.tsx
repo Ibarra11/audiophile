d954246092fe54dfd22816fcb11d020050181ee9
Alan Ibarra <alan.ibarra209@gmail.com>
2021-10-24 00:05:01 -0700

    Product showcase body for each item

--- src/components/ProductShowcase/index.tsx
+++ src/components/ProductShowcase/index.tsx
@@ -7,6 +7,11 @@ import {
   OuterCircle,
   InnerCircle,
   ShowcaseImg,
+  ShowcaseItemTitle,
+  ShowcaseItemText,
+  ShowcaseButton,
+  MainCircle,
+  ShowcaseBody,
 } from './styles';
 import SpeakerProductImg from '../../assets/home/mobile/image-speaker-zx9.png';
 const ProductShowcase = () => {
@@ -14,12 +19,22 @@ const ProductShowcase = () => {
     <ShowcaseWrapper>
       <ShowcaseItem>
         <CircleWrapper>
-          <OuterCircle>
-            <InnerCircle>
-              <ShowcaseImg src={SpeakerProductImg} alt="Speaker" />
-            </InnerCircle>
-          </OuterCircle>
+          <MainCircle>
+            <OuterCircle>
+              <InnerCircle>
+                <ShowcaseImg src={SpeakerProductImg} alt="Speaker" />
+              </InnerCircle>
+            </OuterCircle>
+          </MainCircle>
         </CircleWrapper>
+        <ShowcaseBody>
+          <ShowcaseItemTitle>ZX9 Speaker</ShowcaseItemTitle>
+          <ShowcaseItemText>
+            Upgrade to premium speakers that are phenomenally built to deliver truly remarkable
+            sound.
+          </ShowcaseItemText>
+          <ShowcaseButton>See Product</ShowcaseButton>
+        </ShowcaseBody>
       </ShowcaseItem>
     </ShowcaseWrapper>
   );
--- src/components/ProductShowcase/styles.ts
+++ src/components/ProductShowcase/styles.ts
@@ -6,42 +6,106 @@ const ShowcaseWrapper = styled.section`
 `;
 
 const ShowcaseItem = styled.div`
+  position: relative;
   background-color: var(--dark-orange);
   height: 600px;
   padding: 2px 3px 0 4px;
-  overflow: hidden;
   border-radius: 8px;
+  overflow: hidden;
+  isolation: isolate;
 `;
 
 const CircleWrapper = styled.div`
+  position: relative;
+  z-index: -1;
   display: flex;
-  height: 50%;
+  justify-content: center;
   width: 100%;
+  height: 50%;
+`;
+
+const MainCircle = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  top: -120px;
+  margin: 0 auto;
+  height: 558px;
+  width: 558px;
+  border-radius: 50%;
+  border: 1px solid hsla(0, 0%, 73%, 0.62);
 `;
 
 const OuterCircle = styled.div`
-  position: relative;
-  top: -2%;
-  width: 100%;
-  height: 100%;
+  position: absolute;
+  top: 120px;
+  margin-top: -2%;
+  width: 320px;
+  height: 320px;
   border-radius: 50%;
-  border: 1px solid #ced4da;
-  padding: 20px;
+  border: 1px solid hsla(0, 0%, 73%, 0.62);
 `;
 
 const InnerCircle = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 1px solid red;
-  height: 100%;
-  width: 100%;
+  width: 279px;
+  height: 279px;
   border-radius: 50%;
-  border: 1px solid #ced4da;
+  border: 1px solid hsla(0, 0%, 73%, 0.62);
 `;
 
 const ShowcaseImg = styled.img`
-  width: 50%;
+  width: 55%;
 `;
 
-export { ShowcaseWrapper, ShowcaseItem, CircleWrapper, OuterCircle, InnerCircle, ShowcaseImg };
+const ShowcaseItemTitle = styled.h3`
+  font-size: ${36 / 16}rem;
+  font-weight: 700;
+  line-height: 40px;
+  letter-spacing: 1vw;
+  text-transform: uppercase;
+  margin-top: 0;
+  margin-bottom: 24px;
+`;
+
+const ShowcaseItemText = styled.p`
+  font-size: ${15 / 16}rem;
+  font-weight: 200;
+  line-height: 25px;
+  margin-bottom: 24px;
+`;
+
+const ShowcaseButton = styled.button`
+  /* color: inherit; */
+`;
+
+const ShowcaseBody = styled.div`
+  margin-top: -3%;
+  height: 100%;
+  padding: 0 23px;
+  padding-bottom: 55px;
+  color: var(--white);
+  text-align: center;
+`;
+
+export {
+  ShowcaseWrapper,
+  ShowcaseItem,
+  CircleWrapper,
+  OuterCircle,
+  InnerCircle,
+  ShowcaseImg,
+  ShowcaseItemTitle,
+  ShowcaseItemText,
+  ShowcaseButton,
+  MainCircle,
+  ShowcaseBody,
+};
