91edeb5d97abbd84000c2095b5222938670acb93
Alan Ibarra <alan.ibarra209@gmail.com>
2021-10-24 21:25:16 -0700

    Refactored Product showcase

--- src/components/ProductShowcase/index.tsx
+++ src/components/ProductShowcase/index.tsx
@@ -1,41 +1,13 @@
 import React from 'react';
+import ZX9 from './ZX9';
 import Button from '../Buttons';
-import {
-  CircleWrapper,
-  ShowcaseItem,
-  ShowcaseWrapper,
-  OuterCircle,
-  InnerCircle,
-  ShowcaseImg,
-  ShowcaseItemTitle,
-  ShowcaseItemText,
-  ShowcaseButton,
-  MainCircle,
-  ShowcaseBody,
-} from './styles';
-import SpeakerProductImg from '../../assets/home/mobile/image-speaker-zx9.png';
+
+import { ShowcaseWrapper } from './styles';
+
 const ProductShowcase = () => {
   return (
     <ShowcaseWrapper>
-      <ShowcaseItem>
-        <CircleWrapper>
-          <MainCircle>
-            <OuterCircle>
-              <InnerCircle>
-                <ShowcaseImg src={SpeakerProductImg} alt="Speaker" />
-              </InnerCircle>
-            </OuterCircle>
-          </MainCircle>
-        </CircleWrapper>
-        <ShowcaseBody>
-          <ShowcaseItemTitle>ZX9 Speaker</ShowcaseItemTitle>
-          <ShowcaseItemText>
-            Upgrade to premium speakers that are phenomenally built to deliver truly remarkable
-            sound.
-          </ShowcaseItemText>
-          <Button id={'btn2'}>See Product</Button>
-        </ShowcaseBody>
-      </ShowcaseItem>
+      <ZX9 />
     </ShowcaseWrapper>
   );
 };
