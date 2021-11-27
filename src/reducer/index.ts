3f3a8f47c3eb6d21f713fc67df8af830ff51b6c2
Alan Ibarra <alan.ibarra209@gmail.com>
2021-11-26 21:25:11 -0800

    Updated reducer state with a total for the cart

--- src/reducer/index.ts
+++ src/reducer/index.ts
@@ -5,27 +5,27 @@ import { Cart, Actions, ActionTypes } from '../shared/types';
 export function ShoppingCartReducer(cart: Cart, action: Actions): Cart {
   switch (action.type) {
     case ActionTypes.ADD_PRODUCT: {
-      const { id } = action.payload.product;
+      const { product } = action.payload;
       const productIndex = cart.products.findIndex(
-        (product) => product.id === id,
+        (cartProduct) => cartProduct.id === product.id,
       );
       if (productIndex >= 0) {
         const productsCpy = [...cart.products];
 
         productsCpy[productIndex] = {
           ...productsCpy[productIndex],
-          amount:
-            productsCpy[productIndex].amount +
-            action.payload.product.amount,
+          amount: productsCpy[productIndex].amount + product.amount,
         };
         return {
           products: productsCpy,
-          size: cart.size + action.payload.product.amount,
+          size: cart.size + product.amount,
+          total: cart.total + product.amount * product.price,
         };
       } else {
         return {
-          products: [...cart.products, { ...action.payload.product }],
-          size: cart.size + action.payload.product.amount,
+          products: [...cart.products, { ...product }],
+          size: cart.size + product.amount,
+          total: cart.total + product.amount * product.price,
         };
       }
     }
@@ -43,19 +43,22 @@ export function ShoppingCartReducer(cart: Cart, action: Actions): Cart {
         return {
           products: cartCpy,
           size: cart.size - 1,
+          total: cart.total - productCpy.price,
         };
       } else {
         let cartCpy = [...cart.products];
-        cartCpy.splice(productIndex, 1);
+        const product = cartCpy.splice(productIndex, 1)[0];
+        console.log(product);
         return {
           products: cartCpy,
           size: cart.size - 1,
+          total: cart.total - product.price,
         };
       }
     }
     case ActionTypes.REMOVE_ALL_PRODUCTS: {
       if (cart.size > 0) {
-        return { products: [], size: 0 };
+        return { products: [], size: 0, total: 0 };
       }
       return cart;
     }
