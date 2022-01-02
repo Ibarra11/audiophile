424763f549c008e5cad2885045634d7ea238ce9a
Alan Ibarra <alan.ibarra209@gmail.com>
2022-01-02 01:44:26 -0800

    Galley grid images responsive

--- src/components/Product/Product_Gallery/index.tsx
+++ src/components/Product/Product_Gallery/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import breakpoints from '../../../shared/css/breakpoints';
 interface ProductGalleryProps {
   images: {
     img_1_mb: string;
@@ -10,9 +11,9 @@ interface ProductGalleryProps {
 const ProductGallery = ({ images }: ProductGalleryProps) => {
   return (
     <GridContainer>
-      <GridItem src={images.img_1_mb} />
-      <GridItem src={images.img_2_mb} />
-      <GridItem src={images.img_3_mb} />
+      <GridItem index={0} src={images.img_1_mb} />
+      <GridItem index={1} src={images.img_2_mb} />
+      <GridItem index={2} src={images.img_3_mb} />
     </GridContainer>
   );
 };
@@ -20,10 +21,30 @@ const ProductGallery = ({ images }: ProductGalleryProps) => {
 const GridContainer = styled.div`
   display: grid;
   gap: ${20 / 16}rem;
+  ${breakpoints.tabletAndUp} {
+    grid-template-columns: repeat(2, 1fr);
+  }
 `;
 
-const GridItem = styled.img`
+const GridItem = styled.img<{ index: number }>`
   border-radius: 8px;
+  ${breakpoints.tabletAndUp} {
+    grid-column: ${(p) => {
+      if (p.index < 2) {
+        return 1;
+      } else {
+        return 2;
+      }
+    }};
+    grid-row: ${(p) => {
+      if (p.index === 0) {
+        return 1;
+      } else if (p.index === 1) {
+        return 2;
+      } else {
+        return 'span 2';
+      }
+    }}
 `;
 
 export default ProductGallery;
