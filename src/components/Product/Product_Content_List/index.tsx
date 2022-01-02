2761ccde2af8ec8c6686bc01346ccb6ec9c93e98
Alan Ibarra <alan.ibarra209@gmail.com>
2022-01-02 01:45:42 -0800

    Product features responsive

--- src/components/Product/Product_Content_List/index.tsx
+++ src/components/Product/Product_Content_List/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import breakpoints from '../../../shared/css/breakpoints';
 interface ProductContentListProps {
   contents: { content: string; amount: number }[];
 }
@@ -28,12 +28,36 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: ${24 / 16}rem;
+
+  ${breakpoints.tabletAndUp} {
+    width: 75%;
+    flex-direction: revert;
+    gap: ${12 / 16}rem;
+  }
+  ${breakpoints.laptopAndUp} {
+    width: 100%;
+    flex-direction: column;
+    gap: ${32 / 16}rem;
+  }
 `;
-const Heading = styled.h3``;
+const Heading = styled.h3`
+  ${breakpoints.tabletAndUp} {
+    flex: 1;
+  }
+  ${breakpoints.laptopAndUp} {
+    flex: 0;
+  }
+`;
 const ContentList = styled.ul`
   list-style: none;
   padding: 0;
   margin: 0;
+  ${breakpoints.tabletAndUp} {
+    flex: 1;
+  }
+  ${breakpoints.laptopAndUp} {
+    flex: 0;
+  }
 `;
 const Item = styled.li`
   &:not(:last-of-type) {
@@ -45,7 +69,7 @@ const ItemType = styled.p`
 `;
 const ItemAmount = styled.span`
   color: hsl(var(--clr-primary-orange));
-  font-size: var(--fs-text);
+  font-size: var(--fs-text-body);
   font-weight: 700;
   margin-right: ${21 / 16}rem;
 `;
--- src/components/Product/Product_Features/index.tsx
+++ src/components/Product/Product_Features/index.tsx
@@ -12,15 +12,16 @@ const ProductFeatures = ({ features }: ProductFeaturesProps) => {
     </Wrapper>
   );
 };
-const Wrapper = styled.div`
-  & :not(:last-child) {
-    margin-bottom: ${24 / 16}rem;
-  }
-`;
+const Wrapper = styled.div``;
 
-const Header = styled.h3``;
+const Header = styled.h3`
+  margin-bottom: ${24 / 16}rem;
+`;
 const Text = styled.p`
   color: hsl(var(--clr-primary-black) / 0.5);
+  &:first-of-type {
+    margin-bottom: ${16 / 16}rem;
+  }
 `;
 
 export default ProductFeatures;
